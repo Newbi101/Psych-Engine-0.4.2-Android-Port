bb151de2dad4ecd95fe87df718fb743e2b4e3266
Newbi101 <96915511+Newbi101@users.noreply.github.com>
2021-12-31 22:48:15 +0900

    Delete eddHouse.lua

--- assets/preload/weeks/eddHouse.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-    makeLuaSprite('house','bgHouse',-162.1,-386.1)
-    makeLuaSprite('sky','bgSky',-250.1,-386.1)
-    setLuaSpriteScrollFactor("sky", 0.5, 0)
-    addLuaSprite('sky',false)
-    addLuaSprite('house',false)
-    close(true)
-end
